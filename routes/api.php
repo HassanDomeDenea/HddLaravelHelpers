5231294cf91e67967693ad220e54c9ef775600a3
HassanDomeDenea <hassan.domedenea@gmail.com>
2025-10-18 11:51:34 +0300

    fix update description

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ Route::prefix('api')->group(function () {
     Route::patch('medias/{media}/update-date', [MediaController::class, 'updateDate'])->name('media.update-date')
         ->middleware('auth:sanctum');
 
-    Route::patch('medias/{media}/update-description', [MediaController::class, 'updateDescription'])->name('media.update-date')
+    Route::patch('medias/{media}/update-description', [MediaController::class, 'updateDescription'])->name('media.update-description')
         ->middleware('auth:sanctum');
 
     Route::patch('medias/{media}/manipulate', [MediaController::class, 'manipulate'])->name('media.manipulate')
